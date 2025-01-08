3978880666a75b90997c31f3536b11242174ff60
AnatolyZhukov <t01ya@mail.ru>
2025-01-08 21:20:40 +0200

    We build the storage architecture. Task #1

--- models/trips_stat.sql
+++ models/trips_stat.sql
@@ -1,8 +1,8 @@
 SELECT
-    count(*) AS trips,
-    count(distinct user_id) AS users,
-    avg(extract(epoch from (finished_at - started_at))) / 60 AS avg_duration_m,
-    sum(price) / 100 AS revenue_rub,
-    count(price = 0 or null) / cast(count(*) AS real) * 100 AS free_trips_pct
-FROM
-    scooters_raw.trips
+    count(*) as trips,
+    count(distinct user_id) as users,
+    avg(duration_s) / 60 as avg_duration_min,
+    sum(distance_m) / 1000 as sum_distance_km,
+    sum(price_rub) as revenue_rub,
+    count(is_free or null) / cast(count(*) as real) * 100 as free_trips_pct
+FROM {{ ref("trips_prep") }}
